125852287e2b0a2ae0f8032f46460443d8da4373
Celdama <celdama.dev@gmail.com>
2022-01-06 12:38:04 +0100

    index.: create navbar with jsx

--- index.js
+++ index.js
@@ -92,14 +92,70 @@
 
 ////////////////////////////////////////////////////////////////////////////////////
 // Imperative way to create element
-const title = document.createElement('h1');
-title.textContent = 'This is an imperative way to program';
-title.classList.add('header');
-document.getElementById('root').appendChild(title);
+// const title = document.createElement('h1');
+// title.textContent = 'This is an imperative way to program';
+// title.classList.add('header');
+// document.getElementById('root').appendChild(title);
 
 // Declarative way to create element
-ReactDOM.render(
-  <h1 className='header'>This is an declarative way to program</h1>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <h1 className='header'>This is an declarative way to program</h1>,
+//   document.getElementById('root')
+// );
 ////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////
+// const h1 = document.createElement('h1');
+// h1.textContent = 'Hello World';
+// h1.className = 'header';
+// console.log(h1);
+
+// const element = (
+//   <h1 className='header' title='tt'>
+//     This is JSX
+//   </h1>
+// );
+// console.log(element);
+
+// const page = (
+//   <div>
+//     <h1>this is JSX</h1>
+//     <p>this is a paragraph</p>
+//   </div>
+// );
+
+// console.log(page);
+
+// // JSX = JavaScript XML
+// ReactDOM.render(page, document.getElementById('jsx'));
+
+////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////
+
+/*
+Challenge:
+
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
+*/
+
+const navbar = (
+  <nav>
+    <h1>Celdama</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+);
+
+ReactDOM.render(navbar, document.getElementById('jsx'));
+
+///////////////////////////////////////////////////////////////////////////////////
